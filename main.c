385df318f18ccf3bb0685441107c9a25cff27f3c
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-04-16 12:23:17 -0300

    Estructuras

--- main.c
+++ main.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "funciones.h"
+#include <string.h>
 
+typedef struct
+    {
+        char nombre[20];
+        int legajo;
+        float sueldo;
+        char sexo;
+    }eEmpleado;
+
+void mostrarEmpleado(eEmpleado);
 
 int main()
 {
-    int x[]={34,14,23,12,7};
+    //eEmpleado unEmpleado;
+    eEmpleado unEmpleado={"Juan",1111,1000.75,'m'}; //HARDCODEO, TIENE QUE RESPETAR EL ORDEN DE LA DECLARACION
+    //eEmpleado otroEmpleado; se copio y se igualo otroEmpleado a unEmpleado
+    //otroEmpleado=unEmpleado;
 
-    int c;
+   /* unEmpleado.legajo=1234;
+    strcpy(unEmpleado.nombre,"Juan"); //se tiene que hacer un string copy para copiar una cadena.
+    unEmpleado.sueldo=50000.50;
+    unEmpleado.sexo='m';*/
 
-    printf("0 para ordenar de menor a mayor, 1 para mayor a menor: ");
-    scanf("%d",&c);
+    /*printf("Nombre: \n");
+    gets(unEmpleado.nombre);
+    printf("Legajo: \n");
+    scanf("%d", &unEmpleado.legajo);
+    printf("Sueldo: %\n");
+    scanf("%2.f", &unEmpleado.sueldo);
+    printf("Sexo: \n");
+    fflush(stdin);
+    scanf("%c", &unEmpleado.sexo);*/ //Tipo larga, donde el usuario ingresa los datos
 
-
-    ordenar(x,5,c);
-
-    mostrarVector(x,5);
-
-    return 0;
+    mostrarEmpleado(unEmpleado);
 }
+void mostrarEmpleado(eEmpleado emp)
+{
+    printf("Nombre: %s\n", emp.nombre);
+    printf("Legajo: %d\n", emp.legajo);
+    printf("Sueldo: %.2f\n", emp.sueldo);
+    printf("Sexo: %c \n", emp.sexo);
+}
+
+
