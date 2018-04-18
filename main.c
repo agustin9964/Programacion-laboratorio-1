1233ecfeb8fc06c61fe8caf61ae53ed7e8048382
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-04-18 12:20:24 -0300

    isDigit hecho por mi

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+// VALIDAR QUE SEA UN NUMERO.
+int main()
+{
+   char dato[15];
+   int flag=0,nro;
 
-int main(){
-    char dato[15];
-    int datoInt;
-
-    printf("Ingrese el dato: ");
-    scanf("%s", dato);
-    datoInt = atoi(dato);
-
-    printf("\ndatoInt: %d", datoInt+3);
-
-    sprintf(dato,"%d",datoInt+5);
-
-    printf("\ndato: %s", dato);
-
-    return 0;
+   printf("Ingrese numero: ");
+   fflush(stdin);
+   scanf("%s", dato);
+   for (int i=0; i<strlen(dato);i++)
+   {
+       if(!isdigit(dato[i])) //BUSCA UN NUMERO, SI ESTA NEGANDO BUSCA ALGO QUE NO SEA UN NUMERO si es un digito devuelve 1 en caso normal
+       {
+           flag=1; //EL FLAG TRANSFORMADO A 1 IMPLICA QUE ENCONTRO UN DATO QUE NO ES UN NUMERO
+           break;
+       }
+   }
+   if(flag==0)
+   {
+       nro;atoi(dato);
+   }
 }
