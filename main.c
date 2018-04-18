64d7fc59e195ab29582354a6f9a95bbcc2a5a3ac
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-04-18 12:24:47 -0300

    Estructura Anidada dentro de otra.PROFE

--- main.c
+++ main.c
@@ -1,78 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define CANT 2
-#include <string.h>
 
-/* char n1[30]="jose";
-    char n2[30];
-    n2=n1 ESTA MAL
-    for(i=0;i<30;i++)
-    {
-        n2[i]=n1[i];
-    } TAMPOCO ES VALIDA. SOLO STRING COPY
-    strcpy(n2,n1); SE TIENE QUE USAR STRING COPY*/
+struct fecha{
+    int dia, mes, anio;
+};
 
-typedef struct
-{
-    int dia,mes,anio;
-
-}eFecha; //la e minuscula indica ESTRUCTURA
-typedef struct
-{
-    char nombre[21];
-    int legajo;
-    float sueldo;
-    char sexo;
-    eFecha fechaNacimiento; //llamo a la estructura eFecha.
-}eEmpleado;
-
+struct gente{
+    char nombre[81];
+    struct fecha f_nacimiento;
+};
 
 int main()
 {
-   // eEmpleado empleado; EN CASO DE SER CADENA
+    struct gente persona;
 
-    /*printf("Nombre: \n");
-    gets(empleado.nombre);  //para llamar a la estructura de eEmpleado la funcion NOMBRE
-    printf("Legajo: \n");
-    scanf("%d", &empleado.legajo);
-    printf("Sueldo: \n");
-    scanf("%f", &empleado.sueldo);
-    printf("Sexo: \n");
-    fflush(stdin);
-    scanf("%c", &empleado.sexo);
-    printf("Dia: \n");
-    scanf("%d", &empleado.fechaNacimiento.dia); //LLAMA A LA ESTRUCTURA eEMPLEADO y dentro llama a eFECHA que, toma el dato DIA
-    printf("Mes: \n");
-    scanf("%d", &empleado.fechaNacimiento.mes); */ //EN CASO DE SER CADENA
+    printf("Nombre: ");
+    scanf("%[^\n]", persona.nombre);
 
-    eEmpleado empleado[CANT];//={{"Juan",1111,1000.75,'m',10,5,1969},{"Ana",2222,2000.40,'f',4,11,1972}};
-    eEmpleado aux; //EN CASO DE SER VECTOR
+    printf("\n\nFecha de nacimiento");
+    printf("\nDia: ");
+    scanf("%d", &persona.f_nacimiento.dia);
 
+    printf("\nMes: ");
+    scanf("%d", &persona.f_nacimiento.mes);
 
-    for(int i=0;i<CANT-1;i++)
-    {
-        for(int j=i+1;j<CANT;j++)
-        {
-            if(empleado[i].sueldo< empleado[j].sueldo) //TIENE QUE SER MENOR, PARA QUE SE PUEDA SOPPEAR Y QUEDE ORDENADO DE MAYOR A MENOR.
-            {
-                aux=empleado[i];
-                empleado[i]=empleado[j];
-                empleado[j]=aux;
-            }
-            else
-            {
-                if(empleado[i].sueldo==empleado[j].sueldo)
-                {
-                    if(strcmp(empleado[i].nombre,empleado[j].nombre)>0) //PARA COMPARAR CADENAS Y ORDENAR POR SEGUNDO CRITERIO POR NOMBRE
-                    {
-                        aux=empleado[i];
-                        empleado[i]=empleado[j];
-                        empleado[j]=aux;
-                    }
-                }
-            }
-        }
-    }
+    printf("\nAño: ");
+    scanf("%d", &persona.f_nacimiento.anio);
+
+    system("cls");
+    printf("%s nacio el %d/%d/%d", persona.nombre, persona.f_nacimiento.dia, persona.f_nacimiento.mes, persona.f_nacimiento.anio);
+
     return 0;
-
 }
