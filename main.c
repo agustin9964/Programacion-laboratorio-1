f3ac569b112af2a873cd5d6e4b88c592ca1115f4
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-04-18 12:22:48 -0300

    Primer estructura clase 18/4

--- main.c
+++ main.c
@@ -1,25 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define CANT 2
 #include <string.h>
-// VALIDAR QUE SEA UN NUMERO.
+
+/* char n1[30]="jose";
+    char n2[30];
+    n2=n1 ESTA MAL
+    for(i=0;i<30;i++)
+    {
+        n2[i]=n1[i];
+    } TAMPOCO ES VALIDA. SOLO STRING COPY
+    strcpy(n2,n1); SE TIENE QUE USAR STRING COPY*/
+
+typedef struct
+{
+    int dia,mes,anio;
+
+}eFecha; //la e minuscula indica ESTRUCTURA
+typedef struct
+{
+    char nombre[21];
+    int legajo;
+    float sueldo;
+    char sexo;
+    eFecha fechaNacimiento; //llamo a la estructura eFecha.
+}eEmpleado;
+
+
 int main()
 {
-   char dato[15];
-   int flag=0,nro;
+   // eEmpleado empleado; EN CASO DE SER CADENA
 
-   printf("Ingrese numero: ");
-   fflush(stdin);
-   scanf("%s", dato);
-   for (int i=0; i<strlen(dato);i++)
-   {
-       if(!isdigit(dato[i])) //BUSCA UN NUMERO, SI ESTA NEGANDO BUSCA ALGO QUE NO SEA UN NUMERO si es un digito devuelve 1 en caso normal
-       {
-           flag=1; //EL FLAG TRANSFORMADO A 1 IMPLICA QUE ENCONTRO UN DATO QUE NO ES UN NUMERO
-           break;
-       }
-   }
-   if(flag==0)
-   {
-       nro;atoi(dato);
-   }
+    /*printf("Nombre: \n");
+    gets(empleado.nombre);  //para llamar a la estructura de eEmpleado la funcion NOMBRE
+    printf("Legajo: \n");
+    scanf("%d", &empleado.legajo);
+    printf("Sueldo: \n");
+    scanf("%f", &empleado.sueldo);
+    printf("Sexo: \n");
+    fflush(stdin);
+    scanf("%c", &empleado.sexo);
+    printf("Dia: \n");
+    scanf("%d", &empleado.fechaNacimiento.dia); //LLAMA A LA ESTRUCTURA eEMPLEADO y dentro llama a eFECHA que, toma el dato DIA
+    printf("Mes: \n");
+    scanf("%d", &empleado.fechaNacimiento.mes); */ //EN CASO DE SER CADENA
+
+    eEmpleado empleado[CANT];//={{"Juan",1111,1000.75,'m',10,5,1969},{"Ana",2222,2000.40,'f',4,11,1972}};
+    eEmpleado aux; //EN CASO DE SER VECTOR
+
+
+    for(int i=0;i<CANT-1;i++)
+    {
+        for(int j=i+1;j<CANT;j++)
+        {
+            if(empleado[i].sueldo< empleado[j].sueldo) //TIENE QUE SER MENOR, PARA QUE SE PUEDA SOPPEAR Y QUEDE ORDENADO DE MAYOR A MENOR.
+            {
+                aux=empleado[i];
+                empleado[i]=empleado[j];
+                empleado[j]=aux;
+            }
+            else
+            {
+                if(empleado[i].sueldo==empleado[j].sueldo)
+                {
+                    if(strcmp(empleado[i].nombre,empleado[j].nombre)>0) //PARA COMPARAR CADENAS Y ORDENAR POR SEGUNDO CRITERIO POR NOMBRE
+                    {
+                        aux=empleado[i];
+                        empleado[i]=empleado[j];
+                        empleado[j]=aux;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+
 }
